b959b084700a359b5ba9d8362e9a3edca3a03e9c
mlauter <lauter.miriam@gmail.com>
2014-08-28 12:51:26 -0400

    hacker school ctf

---
+++ thing/ctf.js
@@ -0,0 +1,13 @@
+<html>
+<head></head>
+    <body>
+        <pre>
+
+            // super secret invisible gadget
+            console.log("hello")
+            document.getElementbyId("center").innerHTML = "HackerSchool!";
+
+        </pre>
+    </body>
+
+</html>
